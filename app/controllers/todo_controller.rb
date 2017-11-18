61057e7dc486551e945474d092e4e511fb1c54a8
AlbertoD <aldo10012@gmail.com>
2017-11-18 22:51:03 +0000

    add links

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,7 +2,13 @@ class TodoController < ApplicationController
     def index
     end
     def show
-        @todo_description="make a curriculum"
-        @todo_pomodoro_estimate = 4
+        todo_id = params[:id]
+        if todo_id=='1'
+            @todo_description="make the curriculum"
+            @todo_pomodoro = 4
+        elsif todo_id == '2'
+            @todo_description ="Do HW"
+            @todo_pomodoro_estimate = 6
+        end
     end
 end
